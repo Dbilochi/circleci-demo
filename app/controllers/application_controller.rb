f09141e4933a76b48c693bb8afb599c3e3c0d06d
Deeksha <bilochideeksha@gmail.com>
2019-04-12 14:09:17 +0530

    text changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-   render html: "hello, world!"
+   render html: "hello, world! Hello Deeksha..Hello All"
  end
 end
