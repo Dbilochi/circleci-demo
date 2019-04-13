a817f942fb1ef7bdd35b00e2029ceef470329173
Deeksha <bilochideeksha@gmail.com>
2019-04-13 11:23:02 +0530

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create([{name:'P1',title:'t1',p_no:'1'},{name:'P2',title:'t2',p_no:'2'},name:'P3',title:'t3',p_no:'3'])
